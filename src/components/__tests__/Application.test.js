1336ae22f4d8cd534836f1cb5637b45e43fc8f38
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-06 16:25:45 -0700

    update application unit test

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { 
   render, 
   cleanup,
-  getByText,
+  //getByText,
   waitForElement,
   fireEvent,
 } from "@testing-library/react";
@@ -16,7 +16,8 @@ afterEach(cleanup);
 
 describe("Application", () => {
   it("defaults to Monday and changes the schedule when a new day is selected", () => {
-      const { getByText } = render(<Application />);
+    const { getByText } = render(<Application />);
+    
     return waitForElement(() => getByText("Monday")).then(() => {
       fireEvent.click(getByText("Tuesday"));
       expect(getByText("Leopold Silvers")).toBeInTheDocument();
