b1109a6cc37214961fd4cc7a9d428dce4d5b2890
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-06 15:45:02 -0700

    add interview api mock

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -72,7 +72,11 @@ export default {
     }
 
     if (url === "/api/interviewers") {
-      /* Resolve interviewers data */
+      return Promise.resolve({
+        status: 200,
+        statusText: "OK",
+        data: fixtures.appointments
+      })
     }
   })
 }
