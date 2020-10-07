2fc8f804aee3a4f3782f053a6351a3cbd3471a22
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-07 13:13:55 -0700

    remove unused tests

--- src/components/__tests__/Forms.test.js
+++ src/components/__tests__/Forms.test.js
@@ -48,38 +48,6 @@ describe("Form", () => {
     expect(onSave).not.toHaveBeenCalled();
   });
   
-  // replaced by "can successfully save after trying to submit an empty student name"
-  it.skip("calls onSave function when the name is defined", () => {
-    const onSave = jest.fn();
-    const { queryByText } = render(<Form
-      interviewers={interviewers}
-      onSave={onSave}
-      name='Lydia Miller-Jones'
-      />
-    )
-    fireEvent.click(queryByText("Save"));
-    expect(queryByText(/student name cannot be blank/i)).toBeNull();
-  
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  });
-
-  // replaced by "can successfully save after trying to submit an empty student name"
-  it.skip("submits the name entered by the user", () => {
-    const onSave = jest.fn();
-    const { getByText, getByPlaceholderText } = render(
-      <Form interviewers={interviewers} onSave={onSave} />
-    );
-  
-    const input = getByPlaceholderText("Enter Student Name");
-  
-    fireEvent.change(input, { target: { value: "Lydia Miller-Jones" } });
-    fireEvent.click(getByText("Save"));
-  
-    expect(onSave).toHaveBeenCalledTimes(1);
-    expect(onSave).toHaveBeenCalledWith("Lydia Miller-Jones", null);
-  });
-  
   it("can successfully save after trying to submit an empty student name", () => {
     const onSave = jest.fn();
     const { getByText, getByPlaceholderText, queryByText } = render(
