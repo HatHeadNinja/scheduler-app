ead1eb3e99c01fe76eeb7709d60066632e733797
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-07 08:50:02 -0700

    add descriptions of tests to write in comments

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -75,4 +75,11 @@ describe("Application", () => {
     // console.log(prettyDOM(appointment));
     
   });
+  /* Tests to write
+  "loads data, cancels an interview and increases the spots remaining for Monday by 1"
+  "loads data, edits an interview and keeps the spots remaining for Monday the same"
+  "shows the save error when failing to save an appointment"
+  "shows the delete error when failing to delete an existing appointment"
+  */
+
 })
