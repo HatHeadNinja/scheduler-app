e477add2bde88fe99f75916ce8be2bde3f4c0319
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-07 09:46:27 -0700

    add cypress

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -77,42 +77,40 @@ describe("Application", () => {
     
   });
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
-    // 1. Render the Application
-    const { container, debug } = render(<Application />);
-
-    // 2. Wait until the text "Archie Cohen" is displayed
+    // 1. Render the Application.
+    const { container } = render(<Application />);
+  
+    // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
-
-    // 3. Click "Delete" button on a booked appointment
+  
+    // 3. Click the "Delete" button on the booked appointment.
     const appointment = getAllByTestId(container, "appointment").find(
       appointment => queryByText(appointment, "Archie Cohen")
     );
   
     fireEvent.click(queryByAltText(appointment, "Delete"));
-
-    // 4. Check confirmation message is shown
-    expect(getByText(appointment, "Delete the appointment?")).toBeInTheDocument();
-
-    // 5. Click "Confirm" button on the confirmation dialog
+  
+    // 4. Check that the confirmation message is shown.
+    expect(
+      getByText(appointment, "Are you sure you would like to delete?")
+    ).toBeInTheDocument();
+  
+    // 5. Click the "Confirm" button on the confirmation.
     fireEvent.click(queryByText(appointment, "Confirm"));
-    
-    // MENTOR ASSISTANCE REQ'D
-    // ========================
-    // 6. Check element with the text "Deleting" is being displayed
-    // expect(getByText(appointment, "Deleting")).toBeInTheDocument();
-
-    // 7. Wait until element with "Add" button is being displayed
+  
+    // 6. Check that the element with the text "Deleting" is displayed.
+    expect(getByText(appointment, "Deleting")).toBeInTheDocument();
+  
+    // 7. Wait until the element with the "Add" button is displayed.
     await waitForElement(() => getByAltText(appointment, "Add"));
-
-    // 8. Check DayListItem with the text "Monday" also has text "2 spots remaining"
+  
+    // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
     );
   
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
-    
-    //debug();
-  })
+  });
 })
 /* Tests to write
 "loads data, edits an interview and keeps the spots remaining for Monday the same"
