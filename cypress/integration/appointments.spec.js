0f51adbb6075f7dd1179a8a258ca2dd168f080cc
Unfamous Dyson <hatheadninja@gmail.com>
2020-10-08 00:13:35 -0700

    add book interview test

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -10,5 +10,10 @@ describe("Appointments", () => {
     cy.get('[alt=Add]')
       .first()
       .click()
+    cy.get("[data-testid=student-name-input]").type("Lydia Miller-Jones")
+    cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click()
+    cy.contains('.appointment__card--show', "Lydia Miller-Jones");
+    cy.contains('.appointment__card--show', "Sylvia Palmer");
   });
 })
